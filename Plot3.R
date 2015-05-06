d2637f387534c44f94a6702e491e013a9fba6ab4
yuanh <yuan.huang10@gmail.com>
2015-05-05 20:24:09 -0400

    adding legend to plot3

--- Plot3.R
+++ Plot3.R
@@ -9,8 +9,11 @@ energy$Date_Time <- as.POSIXct(strptime(paste(energy$Date,energy$Time), "%d/%m/%
 
 energy_Jul <- energy %>% filter((Date_Time >= "2007-02-01") & (Date_Time < "2007-02-03"))
 
+op <- par(cex=.64)
+
 with(energy_Jul, plot(y = Sub_metering_1 , x = Date_Time, type = "l",  ylab = "Energy sub metering"))
 with(energy_Jul, lines(y = Sub_metering_2,  x = Date_Time, col = "red"))
 with(energy_Jul, lines(y = Sub_metering_3,  x = Date_Time, col = "blue"))
 
-legend("topright", lty = 1,col = c("black","red","blue"), legend =c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright", lty = 1,col = c("black","red","blue"), 
+       legend =c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
